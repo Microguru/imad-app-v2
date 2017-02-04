87e220e6fbb3509b4ed262c1b271a5e95b5c4032
Microguru <Manishguru26@gmail.com>
2017-02-04 17:56:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1',function(req,res) {
+   res.send('Article1 is requested and will served');
+});
+app.get('/article2',function(req,res) {
+   res.send('Article2 is requested and will served');
+});
+app.get('/article3',function(req,res) {
+   res.send('Article3 is requested and will served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
