155a993dd53e8b0a520064770299e790f8d16cb1
Microguru <Manishguru26@gmail.com>
2017-02-08 21:14:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-var articleOne = {
+var articles= {
+'article-one' :{
     title:'Article-one',
     heading:'Article One',
     date: '7 FEb 2017',
@@ -19,8 +19,27 @@ var articleOne = {
              very excited to write server side script in node.js.
              </p> `
     
+},
+'articletwo':{
+     title:'Article-Two',
+    heading:'Article Two',
+    date: '7 FEb 2017',
+    content:`<p>
+    This the content of my second article 
+    
+    </p>`
+},
+'article-three':{
+    title:'Article-Three',
+    heading:'Article Three',
+    date: '7 FEb 2017',
+    content:
+    `<p>
+       This the content of my third article 
+    
+    </p> `
+}
 };
-
 function createTemplate(data){
     var title=data.title;
     var date= data.date;
@@ -65,14 +84,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res) {
- res.send(createTemplate(articleOne));
-});
-app.get('/article-two',function(req,res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));  
-});
-app.get('/article-three',function(req,res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName',function(req,res) {
+    //articleName=articleOne
+ res.send(createTemplate(article[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
