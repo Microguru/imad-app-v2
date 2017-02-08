a8ef5b22b25ac0d08cfcbeffa207c03db56a9973
Microguru <Manishguru26@gmail.com>
2017-02-08 21:16:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res) {
     //articleName=articleOne
+    var articleName=req.params.articleName;
  res.send(createTemplate(article[articleName]));
 });
 
