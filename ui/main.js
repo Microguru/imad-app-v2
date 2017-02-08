4f5bb8ef96304c97a33ac614f93b30ba86347a23
Microguru <Manishguru26@gmail.com>
2017-02-08 22:52:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
 //change the text of main text
 
-var element =document.getElementById('main-text');
-element.InnerHtml='New Value';
+var element = document.getElementById('main-text');
+
+element.InnerHtml = 'New Value';
