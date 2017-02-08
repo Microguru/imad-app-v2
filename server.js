ac1031dc86892f087093b111dbcf5ff4e2e219f5
Microguru <Manishguru26@gmail.com>
2017-02-09 00:33:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,8 +99,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('ui/DSC_0175.JPG', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'DSC_0175.JPG'));
 });
 
 
