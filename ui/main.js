d90215fe17332894cebffca7165ec34988d41f65
Microguru <Manishguru26@gmail.com>
2017-02-09 02:56:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,18 @@ button.onclick =function() {
    var request=new XMLhttpRequest();
     
     
-    request.onreadystatechange= function(){
+   
+    
+
+    
+};
+//sumit
+var nameInput =document.getElementById('name');
+var name = nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick= function(){
+    
+     request.onreadystatechange= function(){
         if(request.readyState === XMLHttpRequest.DONE){
              if(request.readystatus===200){
                  var names= ['name1','name2','name3'];
@@ -21,28 +32,6 @@ button.onclick =function() {
              }
         }
     };
-    
-  request.open('GET','http://http://microguru.imad.hasura-app.io/counter',true)
+      request.open('GET','http://microguru.imad.hasura-app.io/submit-name?name=' +name , true);
   request.send(null);
-    
-};
-
-var nameInput =document.getElementById('name');
-var name = nameInput.value;
-var submit=document.getElementById('submit_btn');
-submit.onclick= function(){
-    
-    
-    
-    
-    
-    
-    var names= ['name1','name2','name3'];
-    var list="";
-    for(var i=0;i<names.lenght;i++){
-        list +='<li>'+name[i]+'<li>'
-;    }
-    var ul=document.getElementById('namelist');
-    ul.innerHtml= list;
-    
 };
