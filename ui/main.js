0dc377e78f850586d9c1354af135bfad37f6b533
Microguru <Manishguru26@gmail.com>
2017-02-09 22:46:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,26 @@
 console.log('Loaded!');
 
 var button= document.getElementById('counter');
-var counter=3;
 
 button.onclick =function() {
-    
-    
+    //Request
+  var request=new XMLHttpRequest();   
+   //
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+             if(request.status===200){
+    var counter=request.responseText;
+    var span=document.getElementById('count');
+    span.innerHtml =counter.toString(); 
+             }
+        }
+    };
 //
-counter =counter+1;
-var span = document.getElementById('count');
-span.innerHTML =counter.toString();
-    
+
+ request.open('GET','http://microguru.imad.hasura-app.io/counter', true);
+  request.send(null);
+   
     
 };
-alert('check');
+
 
