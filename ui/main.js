edca230b36e4b7b3ccca02d3f28f3aafc619b282
Microguru <Manishguru26@gmail.com>
2017-02-09 21:14:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,25 +16,3 @@ span.innerHtml =counter.toString();
     
 };
 
-var submit=document.getElementById('submit_btn');
-submit.onclick= function(){
-    
-     request.onreadystatechange= function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-             if(request.readystatus===200){
-                 var names= request.responseText;
-                 names =JSON.parse(names);
-    var list="";
-    for(var i=0;i<names.lenght;i++){
-        list +='<li>'+name[i]+'<li>'
-;    }
-    var ul=document.getElementById('namelist');
-    ul.innerHtml= list;
-             }
-        }
-    };
-    var nameInput =document.getElementById('name');
-var name = nameInput.value;
-      request.open('GET','http://microguru.imad.hasura-app.io/submit-name?name=' +name , true);
-  request.send(null);
-};
