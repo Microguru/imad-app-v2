529689f50300f427987adfabfe0dff166abd7a0e
Microguru <Manishguru26@gmail.com>
2017-02-10 00:11:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,9 +29,9 @@ var submit=document.getElementById('submit_btn');
 submit.onclick  = function() {
 
 var names=['name1','name2','name3'];
-var list='';
-    for(var i=0;i<names.lenght;i++){
-        list +='<li>'+name[i]+'<li>';    
+var list= '';
+    for(var i=0;i<names.lenght;i++) {
+        list += '<li>'+ name[i] +'</li>';    
 }
 var ul =document.getElementById('namelist');
 ul.innerHTML =list;
